a23db7008d5cb8d31d2a3f6fa6f1114c2c613f5c
Rio Trilaksono Putro <riotrilaksonop@gmail.com>
2025-09-15 16:32:17 +0700

    feat: added input text for guess

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('GUESS:', guess);
+    if (guess.length !== 5) {
+      console.log('GUESS MUST BE 5 LETTERS');
+      return;
+    }
+    setGuess('');
+  };
+
+  return <div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" pattern="[A-Z]{5}" value={guess} onChange={(event) => setGuess(event.target.value.toUpperCase())} />
+    </form>
+  </div>;
+}
+
+export default GuessInput;
